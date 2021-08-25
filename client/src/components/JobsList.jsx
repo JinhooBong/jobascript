8b832e76e2e7176a91f6dc1cae20c203a89d7e65
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-25 09:40:55 -0700

    adding form for input to save to db

--- client/src/components/JobsList.jsx
+++ client/src/components/JobsList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ApplicationForm from './ApplicationForm.jsx';
 
 const JobListDiv = styled.div`
   background-color:
@@ -11,6 +12,7 @@ const JobsList = () => {
   return (
     <div>
       <h1>Job List Section</h1>
+      <ApplicationForm />
     </div>
   )
 }
