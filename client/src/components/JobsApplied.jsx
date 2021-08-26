344c4cb9b3796b49c5be5e5d957df4ab0032871e
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-26 09:16:46 -0700

    adding delete functionality

--- client/src/components/JobsApplied.jsx
+++ client/src/components/JobsApplied.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import {
   Text,
-  Container
+  Container,
+  CloseButton
 } from '@chakra-ui/react';
 
 const JobsApplied = ({ job }) => {
   return (
-    <Container maxW="75vw" m="5" bgColor="#9AC6C4" p="3" borderRadius="5">
+    <Container display="flex" justifyContent="space-between" maxW="75vw" m="5" bgColor="#9AC6C4" p="3" borderRadius="5">
       <Text color="#fff" fontSize="xl">{job.job_title}, <Text as="i">{job.companyName}</Text>, {job.location}, {job.date_added.substring(0, 10)}</Text>
+      <CloseButton onClick={}/>
     </Container>
   )
 }
